8dd2d23f8822922fabf6357cc7bb7d381e462b30
athenas2021 <lucas.kano@outlook.com>
2021-11-27 12:53:05 -0300

    keep learning...

---
+++ 30-days-of-code/15-linked-lists.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self, topo, data): 
+        if topo is None:
+              topo = Node(data)
+              self.fim = topo
+        else: 
+            node = Node(data)
+            self.fim.next = node
+            self.fim = node
+        return topo
+
+        
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
